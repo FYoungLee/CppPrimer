09d3ef118382152394154949ab1e65a135627b29
FYoungLee <lixleon@foxmail.com>
2016-03-21 22:55:14 +0800

    Update ex3_36.cpp

--- ch03/ex3_36.cpp
+++ ch03/ex3_36.cpp
@@ -9,6 +9,7 @@ using std::endl;
 using std::vector;
 
 // pb point to begin of the array, pe point to end of the array.
+/*
 bool compare(int* const pb1, int* const pe1, int* const pb2, int* const pe2)
 {
     if ((pe1 - pb1) != (pe2 - pb2)) // have different size.
@@ -20,17 +21,34 @@ bool compare(int* const pb1, int* const pe1, int* const pb2, int* const pe2)
 
     return true;
 }
+*/
+bool compare(int * const pb1, int * const pb2)
+{
+	if (sizeof(pb1) != sizeof(pb2)) // have different size.
+		return false;
+	else {
+		for (int i = 0; i < sizeof(pb1); ++i)
+			if (pb1[i] != pb2[i])
+				return false;
+	}
+	return true;
+}
 
 int main()
 {
     int arr1[3] = {0, 1, 2};
     int arr2[3] = {0, 2, 4};
-
+    /*
     if (compare(begin(arr1), end(arr1), begin(arr2), end(arr2)))
         cout << "The two arrays are equal." << endl;
     else
         cout << "The two arrays are not equal." << endl;
-
+    */
+    if (compare(arr1, arr2))
+		cout << "The two arrays are equal." << endl;
+	else
+		cout << "The two arrays are not equal." << endl;
+		
     cout << "==========" << endl;
 
     vector<int> vec1 = {0, 1, 2};
