207c7049dd91c10b1b3c7216c0e7cb9dd3d00619
FYoungLee <lixleon@foxmail.com>
2016-03-22 11:30:01 +0800

    Update ex3_42.cpp

--- ch03/ex3_42.cpp
+++ ch03/ex3_42.cpp
@@ -11,14 +11,13 @@ int main()
 {
     vector<int> ivec{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int int_arr[10];
-
     for (int* i = begin(int_arr); i != end(int_arr); ++i)
         *i = ivec[i - begin(int_arr)];
         
     /*{
-		int *pi = int_arr2;
-		for (int &i : ivec) { *pi = i; ++pi; }
-	}*/
+	int *pi = int_arr2;
+	for (int &i : ivec) { *pi = i; ++pi; }
+    }*/
 
     for (auto i : int_arr) cout << i << " ";
     cout << endl;
